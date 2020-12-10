30a485542c3b3ea94b1cbb9ef3e695eb3fef5919
liulinhui <1249055292@qq.com>
2020-12-11 04:12:03 +0800

    init

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function doRequest() {
 
 //增加阅读量
 function doAddView() {
-    let url='https://wushustars.com/contestants/ta1-m2-malcom-mak-zi-qian/'
+    let url='https://wushustars.com/contestants/ta1-m5-grayson-chan-kai-zen/'
     let userAgent = userAgents[parseInt(Math.random() * userAgents.length)]
     request('get', url)
         .set(
